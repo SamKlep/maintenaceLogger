d541e163dbd7ae9e0213cab1eab4fd25f56e6bfc
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-03-02 20:34:28 -0600

    Implemented redux, store, provider, logs reducer, actions and types

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -17,7 +19,9 @@ const App = () => {
     // Initializes Materialize JS
     M.AutoInit();
   });
-  return <Fragment>
+  return (
+    <Provider store={store}>
+  <Fragment>
     <SearchBar />
     <div className="container">
       <AddBtn />
@@ -27,7 +31,9 @@ const App = () => {
      <TechListModal />
       <Logs />
     </div>
-  </Fragment>;
+  </Fragment>
+  </Provider>
+  );
 };
 
 export default App;
