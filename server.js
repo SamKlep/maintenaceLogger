a5de5150544d958310d33f2e7bb9a7ac407b2d45
SamKlep <shamdemann23@gmail.com>
2020-09-25 22:10:45 -0500

    Production route created.'

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 server.use(middlewares);
 server.use(router);
