8a0436cafb5a12dd218916ae8749c09db83424cb
SamKlep <shamdemann23@gmail.com>
2020-09-25 22:24:48 -0500

    Path to db.json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'build/index.html', 'db.json'));
 });
 //
 app.listen(PORT, function () {
