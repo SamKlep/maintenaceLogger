751524e0515bd24638e836d06bf9b2d76b86c039
SamKlep <shamdemann23@gmail.com>
2020-09-25 22:16:23 -0500

    Returned server to previous state.

--- server.js
+++ server.js
@@ -5,18 +5,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build/index.html'));
 });
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+//
+app.listen(PORT, function () {
+  console.log('server listening on port:' + PORT);
+});
