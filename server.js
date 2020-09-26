643f3033835449f78140a326d8fcf820b97ee3c5
SamKlep <shamdemann23@gmail.com>
2020-09-25 22:34:44 -0500

    Path to db.json removed.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html', 'db.json'));
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 //
 app.listen(PORT, function () {
