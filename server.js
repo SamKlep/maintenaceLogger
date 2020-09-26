9fdf823f00efabddd7f9610e10c32cb55253e4f5
SamKlep <shamdemann23@gmail.com>
2020-09-25 22:56:02 -0500

    Added techs and logs endpoints.

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build/index.html'));
 });
-//
+//logs endpoint
+app.get('/logs', function (req, res) {
+  res.send('logs.json');
+});
+// techs endpoint
+app.get('/techs', function (req, res) {
+  res.send('techs.json');
+});
 app.listen(PORT, function () {
   console.log('server listening on port:' + PORT);
 });
