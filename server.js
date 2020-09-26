ee71efba17a54112fd0c5d86e8ed172fc7084f90
SamKlep <shamdemann23@gmail.com>
2020-09-25 22:00:36 -0500

    Server now mock json-server api.

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 5000;
 
+server.use(middlewares);
+server.use(router);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
-});
-//
-app.listen(PORT, function () {
-  console.log('server listening on port:' + PORT);
-});
+server.listen(port);
